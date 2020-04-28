37fe57571998279c53f6c9ac48e4fdeb2fc61194
Michael Fearn <fearn.michael@gmail.com>
2020-04-27 18:13:03 -0600

    Updates

---
+++ precache-manifest.d6e6e8ec57ffcacd02a07ff7755dfb71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78471fe01393df69617e60da777a776a",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "5e5da63dd746620b8977",
+    "url": "/game-of-life/static/css/main.a26beee7.chunk.css"
+  },
+  {
+    "revision": "39a23294104d7a664c68",
+    "url": "/game-of-life/static/js/2.3f9fb196.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-of-life/static/js/2.3f9fb196.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e5da63dd746620b8977",
+    "url": "/game-of-life/static/js/main.588946ed.chunk.js"
+  },
+  {
+    "revision": "51df4f6466fc46c339a1",
+    "url": "/game-of-life/static/js/runtime-main.622907b2.js"
+  }
+]);
